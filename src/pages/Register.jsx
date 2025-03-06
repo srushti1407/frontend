141cc81cbacd625710623f71e28c6126bde58037
srushti1407 <srushtipg005@gmail.com>
2025-03-06 17:04:03 +0530

    adding all files

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,7 +11,7 @@ const Register = ({ setIsLoggedIn }) => {
   const navigate = useNavigate();
 
   const handleRegister = async () => {
-    await axios.post("http://localhost:5000/register", { username, email, password });
+    await axios.post("https://backend-19uu.onrender.com/register", { username, email, password });
     localStorage.setItem("username", username);
     setIsLoggedIn(true);
     navigate("/home");
